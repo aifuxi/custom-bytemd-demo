8bb6128f6899f2cd5e654860e11309461de276d9
aifuxi <aifuxi.js@gmail.com>
2023-12-29 00:08:20 +0800

    feat: config base to deploy github page

--- src/router.tsx
+++ src/router.tsx
@@ -6,31 +6,34 @@ import { PATHS } from './constants/path';
 import ByteMDView from './views/bytemd-editor-view';
 import PreviewContentView from './views/preview-content-view';
 
-export const router = createBrowserRouter([
-  {
-    path: PATHS.HOME,
-    element: (
-      <MainLayout>
-        <ByteMDView />
-      </MainLayout>
-    ),
-  },
-  {
-    path: PATHS.PREVIEW1,
-    element: (
-      <MainLayout>
-        {/* 这里给个唯一key，强制让Preview组件重新渲染，防止组件复用导致useEffect失效 */}
-        <PreviewContentView key={PATHS.PREVIEW1} content={mockContent} />
-      </MainLayout>
-    ),
-  },
-  {
-    path: PATHS.PREVIEW2,
-    element: (
-      <MainLayout>
-        {/* 这里给个唯一key，强制让Preview组件重新渲染，防止组件复用导致useEffect失效 */}
-        <PreviewContentView key={PATHS.PREVIEW2} content={mockContent2} />
-      </MainLayout>
-    ),
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: PATHS.HOME,
+      element: (
+        <MainLayout>
+          <ByteMDView />
+        </MainLayout>
+      ),
+    },
+    {
+      path: PATHS.PREVIEW1,
+      element: (
+        <MainLayout>
+          {/* 这里给个唯一key，强制让Preview组件重新渲染，防止组件复用导致useEffect失效 */}
+          <PreviewContentView key={PATHS.PREVIEW1} content={mockContent} />
+        </MainLayout>
+      ),
+    },
+    {
+      path: PATHS.PREVIEW2,
+      element: (
+        <MainLayout>
+          {/* 这里给个唯一key，强制让Preview组件重新渲染，防止组件复用导致useEffect失效 */}
+          <PreviewContentView key={PATHS.PREVIEW2} content={mockContent2} />
+        </MainLayout>
+      ),
+    },
+  ],
+  { basename: '/custom-bytemd-demo/' },
+);
